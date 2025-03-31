f3c60c247e72c5498587be2bbf06128c1deb2423
Rishabh Tyagi <rishabh.tyagi2311@gmail.com>
2025-03-31 20:31:18 +0530

    nothing

---
+++ apps/server/src/routers/infoRegisterRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import client from "@multisport/db/client"
+
+const router = Router()
+
+router.post("/infoRegister", async(req, res) => {
+
+})
---
+++ apps/server/src/types/index.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const infoRegistertype = z.object({
+    
+
+})
