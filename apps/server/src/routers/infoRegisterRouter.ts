e28eee0e1c0283df7222e965d150c08f6f22aa65
Rishabh Tyagi <rishabh.tyagi2311@gmail.com>
2025-04-01 20:52:22 +0530

    nothing much

--- apps/server/src/routers/infoRegisterRouter.ts
+++ apps/server/src/routers/infoRegisterRouter.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import client from "@multisport/db/client"
+import { infoRegistertype } from "../types";
 
 const router = Router()
 
 router.post("/infoRegister", async(req, res) => {
+    
+    const parsedData = infoRegistertype.safeParse(req.body)
+    if(!parsedData.success)
+    {
+        res.status(400).json({message:"Invalid data format"})
+        return
+
+    }
+    
 
 })
--- apps/server/src/types/index.ts
+++ apps/server/src/types/index.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
 
 export const infoRegistertype = z.object({
+    firstname: z.string(),
+    lastname : z.string(),
+    username: z.string(),
+    age : z.number(),
+    city: z.string(),
+    email: z.string().email()
     
 
+
 })
+
+
